069251da455551c4a37dbdfded90f83b132b2491
Joonas <joonasluhtaniemi@kamk.fi>
2018-01-30 14:09:09 +0200

    Kysymys toimii

---
+++ Prinsessaudelmat/Assets/Scripts - proto/TextControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextControl : MonoBehaviour {
+
+
+
+    List<string> questions = new List<string>()
+    {
+        //  First question
+        "This is the first question."
+    };
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<TextMesh>().text = questions[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
